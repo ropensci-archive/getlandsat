b9bf79c6d57641870923de4a4220ae36a4535993
Scott Chamberlain <myrmecocystus@gmail.com>
2015-09-30 11:20:45 -0700

    added missing params in lsat_image

--- R/lsat_image.R
+++ R/lsat_image.R
@@ -2,6 +2,8 @@
 #'
 #' @export
 #' @param x (character) A file name for a geotif file, will be more general soon.
+#' @param path (character) Path to content to.
+#' @param overwrite	(logical) Will only overwrite existing path if \code{TRUE}
 #' @param ... Curl args passed on to \code{\link[httr]{GET}}
 #' @examples \dontrun{
 #' # pass an image name
